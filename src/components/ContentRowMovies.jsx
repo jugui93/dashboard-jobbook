5b7d2836d2561fa195f137e39a8882cb0dcb6f87
= <=>
2023-01-25 23:56:34 -0500

    Revert "Revert "first commit""
    This reverts commit 7063d1fbb68dad109b505746b44d00fd457f2512.

--- src/components/ContentRowMovies.jsx
+++ src/components/ContentRowMovies.jsx
@@ -1,24 +1,85 @@
 import SmallCard from "./SmallCard";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 
 export default function ContentRowMovies(props){
+
+    const [usuarios, setUsuarios] = useState(0);
+    const [servicios, setServicios] = useState(0);
+    const [categorias, setCategorias] = useState(0);
+    const [imagenes, setImagenes] = useState(0);
+    const [preguntas, setPreguntas] = useState(0);
+    const [respuestas, setRespuestas] = useState(0);
+
+    let fetchData = async (url) => {
+        let data = await fetch(url);
+        let response = await data.json();
+        return response.count
+        
+    };
+    const API = 'http://localhost:3050/api'
+    useEffect( () => {
+        
+        let totales = async (urlApi) => {
+            try{
+            let totalUsuarios = await fetchData(`${urlApi}/usuarios`);
+            let totalServicios = await fetchData(`${urlApi}/servicios`);
+            let totalCategorias = await fetchData(`${urlApi}/servicios/categorias`);
+            let totalImagenes = await fetchData(`${urlApi}/servicios/imagenes`);
+            let totalPreguntas = await fetchData(`${urlApi}/servicios/preguntas`);
+            let totalRespuestas = await fetchData(`${urlApi}/servicios/respuestas`);
+            setUsuarios(totalUsuarios);
+            setServicios(totalServicios);
+            setCategorias(totalCategorias);
+            setImagenes(totalImagenes);
+            setPreguntas(totalPreguntas);
+            setRespuestas(totalRespuestas);
+            } catch (error) { 
+                console.error(error)
+            }
+        }
+        totales(API);
+
+    }, []
+    );
+
     let metricas =[
         {
-            titulo:'Movies in Data Base',
-            color:'primary',
-            cifra:21,
-            icon:'fa-clipboard-list'},
+            titulo: 'Usuarios',
+            color: 'primary',
+            cifra: usuarios,
+            icon: 'fa-users'
+        },
         {
-            titulo:'Total Awards',
-            color:'success',
-            cifra:79,
-            icon:'fa-award'},
+            titulo: 'Servicios',
+            color: 'success',
+            cifra: servicios,
+            icon: 'fa-clipboard-list'
+        },
         {
-            titulo:'Actors Quantity',
-            color:'warning',
-            cifra:49,
-            icon:'fa-user-check'}
+            titulo: 'Categorias',
+            color: 'warning',
+            cifra: categorias,
+            icon: 'fa-object-group'
+        },
+        {
+            titulo: 'Imagenes',
+            color: 'primary',
+            cifra: imagenes,
+            icon: 'fa-images'
+        },
+        {
+            titulo: 'Preguntas',
+            color: 'success',
+            cifra: preguntas,
+            icon: 'fa-question'
+        },
+        {
+            titulo: 'Respuestas',
+            color: 'warning',
+            cifra: respuestas,
+            icon: 'fa-reply-all'
+        }
     ];
     return (
     <div className="row">
